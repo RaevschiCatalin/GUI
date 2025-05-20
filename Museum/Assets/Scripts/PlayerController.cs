da74eb91d8117922e0ab73dc216255d48e1b4fe8
RaevschiCatalin <raevschicatalin@gmail.com>
2025-05-20 13:41:33 +0300

    main menu fully done buttons working + museum camera movement and wasd bind

---
+++ Museum/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float lookSensitivity = 0.2f;
+
+    public Transform playerCamera;
+
+    private PlayerInputActions input;
+    private CharacterController controller;
+
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+
+    private float verticalLookRotation = 0f;
+
+    void Awake()
+    {
+        input = new PlayerInputActions();
+
+        input.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        input.Player.Look.performed += ctx => lookInput = ctx.ReadValue<Vector2>();
+        input.Player.Look.canceled += ctx => lookInput = Vector2.zero;
+    }
+
+    void OnEnable() => input.Enable();
+    void OnDisable() => input.Disable();
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleLook();
+    }
+
+    void HandleMovement()
+    {
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    void HandleLook()
+    {
+        verticalLookRotation -= lookInput.y * lookSensitivity;
+        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
+
+        playerCamera.localRotation = Quaternion.Euler(verticalLookRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * lookInput.x * lookSensitivity);
+    }
+}
