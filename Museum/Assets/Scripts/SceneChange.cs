61c1c46850ea07f11a889eb72ae49aed10f6511a
RaevschiCatalin <raevschicatalin@gmail.com>
2025-06-10 13:04:43 +0300

    black sphere shadow bug fix

--- Museum/Assets/Scripts/SceneChange.cs
+++ Museum/Assets/Scripts/SceneChange.cs
@@ -5,8 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    public GameObject panel;
     public void LoadScene()
     {
+        if(panel != null)
+        {
+            Destroy(panel);
+        }
         SceneManager.LoadScene("Museum");
     }
 }
